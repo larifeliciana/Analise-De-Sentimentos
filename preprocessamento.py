74bd913582e1e048d96db097b1ccc9b92b9d0e14
Larissa Feliciana <larissafelicianasilva@gmail.com>
2018-09-18 18:02:25 -0300

    função de ler e salvar

--- preprocessamento.py
+++ preprocessamento.py
@@ -1,1 +1,10 @@
-#preprocessamentos aqui
+import pickle
+
+def ler(endereco):
+    arq = open(endereco, 'rb')
+    return pickle.load(arq)
+
+
+def salvar(endereco, lista):
+    arq = open(endereco, 'wb')
+    pickle.dump(lista, arq)
