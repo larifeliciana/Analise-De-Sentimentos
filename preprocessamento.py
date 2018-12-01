2c35eb050fc69feddf0204b5e09e776906c0d872
Larissa Feliciana <larissafelicianasilva@gmail.com>
2018-12-01 11:34:51 -0300

    feature selection

--- preprocessamento.py
+++ preprocessamento.py
@@ -3,6 +3,7 @@ from sklearn import feature_extraction
 from sklearn import naive_bayes
 from sklearn import linear_model
 from sklearn import metrics
+from sklearn import feature_selection
 
 def ler(endereco):
     arq = open(endereco, 'rb')
@@ -13,19 +14,33 @@ def salvar(endereco, lista):
     arq = open(endereco, 'wb')
     pickle.dump(lista, arq)
 
-def preprocessamento(treino, teste, tipo, stopwords, max_features):
-#dataset, o tipo da representação, se possui pontuações, a máxima frequência, a mínima frequência, o maximo de features
+def feature_extraction_methods(treino, teste, tipo, stopwords, smooth):
     if stopwords:
         stopwords='english'
 
     if tipo is 'tfidf':
-        pre = feature_extraction.text.TfidfVectorizer(stop_words=stopwords,max_features=max_features)
-    elif tipo is 'tf':
-        pre = feature_extraction.text.CountVectorizer(stop_words=stopwords,max_features=max_features)
+        pre = feature_extraction.text.TfidfVectorizer(stop_words=stopwords,smooth_idf = smooth)
+    elif tipo is 'idf':
+        pre = feature_extraction.text.TfidfVectorizer(stop_words=stopwords, binary=True)
+    elif tipo is 'counter':
+        pre = feature_extraction.text.CountVectorizer(stop_words=stopwords)
     elif tipo is 'binario':
-        pre = feature_extraction.text.CountVectorizer(stop_words=stopwords,max_features=max_features, binary=True)
+        pre = feature_extraction.text.CountVectorizer(stop_words=stopwords)
+    else: pre = feature_extraction.text.BaseEstimator
 
     return pre.fit_transform(treino), pre.transform(teste)
+
+def feature_selection_methods(treino, classes, teste, metodo, k):
+    if metodo is "chi":
+        funct = feature_selection.SelectKBest(feature_selection.chi2, k=k)
+    elif metodo is "anova":
+        funct = feature_selection.SelectKBest(feature_selection.f_classif, k=k)
+    else: return treino, teste
+
+    treino = funct.fit_transform(treino, classes)
+    teste = funct.transform(teste)
+
+    return treino, teste
 
 
 def modelo(treino, treino_classe, tipo):
@@ -33,6 +48,7 @@ def modelo(treino, treino_classe, tipo):
         modelo = naive_bayes.MultinomialNB()
     elif tipo is 'logistic':  # Regressão Logística
         modelo = linear_model.LogisticRegression()
+#    elif tipo is
 
     x = modelo.fit(treino,treino_classe )
     return x
@@ -44,13 +60,25 @@ def teste(teste, modelo): #retorna as classes dos documentos de teste
 def avaliacao(metrica, classes, teste):
     if metrica is 'acuracia':
         return metrics.accuracy_score(y_true=classes, y_pred=teste)
+    if metrica is 'f-mesure':
+        return metrics.f1_score(y_true=classes, y_pred=teste)
 
 
-treino1, treino_classes, teste1, teste_classes = ler('movies')
 
-treino1, teste1 = preprocessamento(treino1, teste1, 'tfidf', True, 3000)
+def main(algoritmo, feature_extraction1, metrica, smooth, feature_selection1,  n_features):
 
-modelo1 = modelo(treino1,treino_classes, 'logistic')
-predito = teste(teste1,modelo1)
+    treino1, treino_classes, teste1, teste_classes = ler('movies')
 
-print(avaliacao('acuracia', teste_classes,predito))
+    treino1, teste1 = feature_extraction_methods(treino1, teste1, feature_extraction1, True, smooth)
+
+    treino1, teste1 = feature_selection_methods(treino1, treino_classes, teste1, feature_selection1 ,n_features)
+
+    modelo1 = modelo(treino1,treino_classes,algoritmo)
+
+    predito = teste(teste1,modelo1)
+
+    print(avaliacao(metrica, teste_classes,predito))
+
+main("logistic", "idf", "acuracia", True, "chi", 5000)
+
+#parametros_grid = {'algoritmo':['logistic', 'naive'], 'feature_extraction1':['tfidf', 'binario', 'counter'],'feature_selection1':['chi', 'anova'] 'n_features':[i for i in range(100, 6000, 500)]}
