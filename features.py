301bafcf856b3663aec9dc987659ecc648990c93
Larissa Feliciana <larissafelicianasilva@gmail.com>
2018-12-25 12:45:12 -0300

    'delta'

--- features.py
+++ features.py
@@ -1,7 +1,7 @@
 
 from sklearn import feature_extraction
 from sklearn import feature_selection
-
+import delta
 
 def feature_extraction_methods(treino, teste, tipo, stopwords, smooth):
     if stopwords:
@@ -16,11 +16,13 @@ def feature_extraction_methods(treino, teste, tipo, stopwords, smooth):
         pre = feature_extraction.text.CountVectorizer(stop_words=stopwords)
     elif tipo is 'binario':
         pre = feature_extraction.text.CountVectorizer(stop_words=stopwords)
+    elif tipo is 'delta':
+        pre = delta.TfidfVectorizer()
 
-    return pre.fit_transform(treino), pre.transform(teste)
+    return pre.fit_transform(treino), pre.transform(teste)#, pre.get_feature_names()
 
 
-def feature_selection_methods(treino, classes, teste, metodo, k):
+def feature_selection_methods(treino, classes, teste, metodo, k, features):
     # Selecionar os k melhores ranqueados de acordo com o método
     if metodo is "chi":
         funct = feature_selection.SelectKBest(feature_selection.chi2, k=k)
@@ -30,6 +32,11 @@ def feature_selection_methods(treino, classes, teste, metodo, k):
 
     treino = funct.fit_transform(treino, classes)
     teste = funct.transform(teste)
+
+ #   cols = funct.get_support(indices=True)
+    # Create new dataframe with only desired columns, or overwrite existing
+  #  for i in cols:
+   #     print(features[i])
 
     return treino, teste
 
